be4141dbaf201b265128344dd23ab215951dd013
Ernest Jezionek <ernestjezionek@macbookair.home>
2026-03-22 21:23:16 +0100

    fix: use SSE proxy for aisstream.io

---
+++ pages/api/ais-stream.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./auth/[...nextauth]"
+import WebSocket from "ws"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession(req, res, authOptions)
+  if (!session) return res.status(401).end()
+
+  const apiKey = process.env.AISSTREAM_KEY
+  if (!apiKey) return res.status(500).end()
+
+  res.setHeader("Content-Type", "text/event-stream")
+  res.setHeader("Cache-Control", "no-cache")
+  res.setHeader("Connection", "keep-alive")
+  res.flushHeaders()
+
+  const ws = new WebSocket("wss://stream.aisstream.io/v0/stream")
+
+  ws.on("open", () => {
+    ws.send(JSON.stringify({
+      APIKey: apiKey,
+      BoundingBoxes: [[[54.5, 17.0], [55.2, 18.5]]],
+      FilterMessageTypes: ["PositionReport"]
+    }))
+  })
+
+  ws.on("message", (data) => {
+    res.write(`data: ${data}\n\n`)
+  })
+
+  ws.on("close", () => res.end())
+  ws.on("error", () => res.end())
+
+  req.on("close", () => ws.close())
+}
